6eb7d2905aa4690f6f8409912c61bb7afb705b77
Koh Zi Chun <zichun@gmail.com>
2013-01-20 08:33:39 +0800

    merge

--- public/js/templates/template.js
+++ public/js/templates/template.js
@@ -1,7 +1,6 @@
 /*global define:true *//*jshint  multistr:true*/
 define(['underscore'], function(_) {
 
-<<<<<<< HEAD
     // ## Templates
     var tmpl = {};
 
@@ -16,9 +15,15 @@ define(['underscore'], function(_) {
         </p>\
     ";
 
+
     tmpl.doc_full = "\
-        <h3 class='key'><%- key %></h3>\
-        <pre class='value'><%= value %></pre>\
+    <div class='optionsbar'>\
+        <a class='option editoption'>edit</a>\
+        &nbsp;|&nbsp;\
+        <a class='option deleteoption'>delete</a>\
+    </div>\
+    <h3 class='key'><%- key %></h3>\
+    <pre class='value'><%= value %></pre>\
     ";
 
     tmpl.doc_collapsed = "\
@@ -62,65 +67,5 @@ define(['underscore'], function(_) {
         compiled[key] = _.template(tmpl[key]);
     });
     return compiled;
-=======
-// ## Templates
-var tmpl = {};
 
-tmpl.log = "\
-<p class='log log-<%- type %>'>\
-	<b>\
-		<small class='count'>\
-			<%- count %>\
-		</small>\
-	</b>\
-	<%- log %>\
-</p>\
-";
-
-tmpl.doc_full = "\
-<div class='optionsbar'>\
-	<a class='option editoption'>edit</a>\
-	&nbsp;|&nbsp;\
-	<a class='option deleteoption'>delete</a>\
-</div>\
-<h3 class='key'><%- key %></h3>\
-<pre class='value'><%= value %></pre>\
-";
-
-tmpl.doc_collapsed = "\
-<span class='key'><%- key %></span>\
-&nbsp;\
-<span class='value'><%- trunc %></span>\
-";
-
-tmpl.db = "\
-<h2><%- db_name %></h2>\
-<small>(database name)</small>\
-<p>\
-	<b>doc_count: </b>\
-	<%- doc_count %>\
-</p>\
-<p>\
-	<b>update_seq: </b>\
-	<%- update_seq %>\
-</p>\
-<div id='toolbar'>\
-    <a class='button' id='query'>Query</a>\
-	&nbsp;|&nbsp;\
-	<a class='button' id='adddoc'>Add document</a>\
-</div>\
-<div id='tabs'>\
-<div class='tab'>\
-<div id='docs'></div>\
-</div>\
-</div>\
-";
-
-// ## Compile templates/partials
-var compiled = {};
-_.each(_.keys(tmpl), function(key){
-    compiled[key] = _.template(tmpl[key]);
-});
-return compiled;
->>>>>>> e2ddaa25dd4c97a4f45914d0849a39ce862a67f5
 });
