99baf060c0fee31c9d68aa0497edc74612c6b692
Koh Zi Chun <zichun@gmail.com>
2013-03-05 16:35:29 +0800

    added hiding option

--- public/js/scripts/start.js
+++ public/js/scripts/start.js
@@ -2,16 +2,24 @@
 /*jshint multistr:true*/
 $(function() {
 	// create divs
-var markup = "\
-<div id='puton-container'>\
-    <h1>Puton</h1>\
-    <div id='puton-main'>\
-        <b><label for='db'>db name: </label></b>\
-        <input type='text' id='db'/>\
-    </div>\
-    <div id='log'></div>\
-</div>";
+
+var markup = 
+"<div id='puton-container'>" +
+    "<h1>Puton</h1>" +
+    "<div id='puton-main'>" +
+        "<b><label for='db'>db name: </label></b>" +
+        "<input type='text' id='db'/>" +
+    "</div>" +
+    "<a href='#' id='hide-button'>Close</a>" +
+    "<div id='log'></div>" +
+"</div>";
+
 	$(markup).appendTo($("body"));
 	var x = new App();
 	x.start();
+
+    $('#hide-button').click(function(e) {
+        $("#puton-container").hide();
+        return false;
+    });
 });
